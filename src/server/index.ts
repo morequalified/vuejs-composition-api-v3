e00b8ab92b8dd92f6272cda7923186d1809e9411
Denys Kulyk <morequalified@gmail.com>
2024-05-14 21:13:12 +0200

    Add Post creation endpoint

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,11 +3,21 @@ import { today, thisWeek, thisMonth, Post } from "../posts"
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const bodyParser = require('body-parser')
  
 app.use(cors())
+app.use(bodyParser.json())
+
+const allPosts = [today, thisWeek, thisMonth]
 
 app.get('/posts', function (_req: any, res: { json: (arg0: Post[]) => void }, _next: any) {
   res.json([today, thisWeek, thisMonth])
+})
+
+app.post("/posts", (req: any, res: { json: (arg0: Post[]) => void }) => {
+  const post = {...req.body, id: (Math.random() * 100000).toFixed() }
+  allPosts.push(post)
+  res.json(post)
 })
  
 app.listen(8000, function () {
